1f26c9104fc57fc9e66929515355c53580175f64
CalebFrederick <caleb@heckfamily.com>
2022-09-28 04:12:52 -0400

    weakness info mid-update

--- src/views/TakedownView.vue
+++ src/views/TakedownView.vue
@@ -8,6 +8,10 @@ export default {
       types: [],
       input: "",
       sprite: "",
+      type1: "",
+      type2: "",
+      damage_from_1: [],
+      damage_from_2: "",
     };
   },
   created: function () {},
@@ -19,6 +23,10 @@ export default {
           console.log(response.data);
           this.types = response.data.types;
           this.sprite = response.data.sprite;
+          this.type1 = response.data.type1;
+          this.type2 = response.data.type2;
+          this.damage_from_1 = response.data.from;
+          this.damage_from_2 = response.data.damage_from_2;
         })
         .catch((error) => {
           this.errors = error.response.data.errors;
@@ -34,6 +42,9 @@ export default {
     <button v-on:click="takeDown()">Takedown Pokemon</button>
     <img :src="sprite" />
     <h2>{{ types }}</h2>
+    <h3>{{ type1 }}</h3>
+    <h3>{{ type2 }}</h3>
+    <h3>{{ damage_from_1 }}</h3>
     <!-- <img :src="sprite" width="300" height="300" /> -->
     <img src="../assets/Type_Chart.png" height="450" />
   </div>
